f04282b66749a44f9ff9bd772fa592eb74085f34
Ravi Prakash <raviprakash.xpd54@gmail.com>
2024-11-01 18:42:06 +0800

    Add methods to run mock_test.cpp

--- Test/test/mockTest/mock_tertle.h
+++ Test/test/mockTest/mock_tertle.h
@@ -1,6 +1,10 @@
 #include "Turtle.h"
 #include <gmock/gmock.h> // Brings in gMock.
 
+// Here MockTurtle we have defined it as mock to test Painter.
+// The mocked version of Turtle allows us to not look at the turtle
+// implementation but just check .h
+
 class MockTurtle : public Turtle {
 public:
   MOCK_METHOD(void, PenUp, (), (override));
